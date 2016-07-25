e3b635a7f5ea1964c079fc5c32db986a07db8414
anand k <anandkm.work@gmail.com>
2016-07-25 15:14:49 +0530

    used docstrings instead of comments

--- coinflipper.py
+++ coinflipper.py
@@ -1,25 +1,31 @@
-#---~~~COIN FLIPPING PROGRAM- WHEN YOU DONT WANT TO WASTE TIME FLIPPING COINS ~~~---
-#init the coin flip
-#repeat it ten times
-#flip a bool statement if a coin flips ten times
-#use an if statement to check whether one coin flipped
-#compare successful runs over total runs
+"""---~~~COIN FLIPPING PROGRAM- WHEN YOU DONT WANT TO WASTE TIME FLIPPING COINS 
+init the coin flip
+repeat it ten times
+flip a bool statement if a coin flips ten times
+use an if statement to check whether one coin flipped
+compare successful runs over total runs
+"""
 import numpy as np
 headortail = np.array([1,2])
-class coin(object): #coin object with the flip function
+class coin(object): 
+    """coin object with the flip function"""
     def __init__(self,checkheads=0):
         self.checkheads = checkheads   
     def coinflip(self, number):
-        for i in range(0,number): #random variable coinz to store values for heads or tails
+        for i in range(0,number): 
+            """random variable coinz to store values for heads or tails"""
             coinz = np.random.choice(headortail)
             if coinz == 1:
                 self.checkheads = self.checkheads + 1
-        return self.checkheads #checkheads returns number of heads
+        return self.checkheads 
+        """checkheads returns number of heads"""
 
-class arrayofcoins(object): #stacks multiple coins to form the group required for the problem
+class arrayofcoins(object): 
+    """stacks multiple coins to form the group required for the problem"""
     def __init__(self, number):
         self.number = number
-    def successnumber(self): #creates the coin flipping run. If one coin gets flipped correctly ten times no point continuing as regardless of the rest of the outcomes the output is 1, so break
+    def successnumber(self): 
+        """creates the coin flipping run. If one coin gets flipped correctly ten times no point continuing as regardless of the rest of the outcomes the output is 1, so break"""
         for x in range(0,self.number):
             coinx = coin()
             if coinx.coinflip(10) == 10:
@@ -29,11 +35,13 @@ class arrayofcoins(object): #stacks multiple coins to form the group required fo
             else:
                 del coinx
 
-class dirtyworkdoer(object): #the object which actually flips the coins and calculates the practical probability
+class dirtyworkdoer(object): 
+    """the object which actually flips the coins and calculates the practical probability"""
     def __init__(self, trials):
         self.trials = trials
         self.successruns = 0
-    def probability(self): #does the multiple runs required, and gives a float number for practical probability
+    def probability(self): 
+        """does the multiple runs required, and gives a float number for practical probability"""
         batcoins = arrayofcoins(1000) #code can be messed with to make a custom value for x in arrayofcoins(x), but theoretical probability calculation must also be changed
         for m in range(0,self.trials):
             if batcoins.successnumber() == 1:
